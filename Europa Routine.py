cba3271786e222e48782884378c272e575664075
esosin <ewalk248@gmail.com>
2018-11-01 13:58:36 -0700

    Updated ROI placement

--- Europa Routine.py
+++ Europa Routine.py
@@ -20,7 +20,8 @@ import select
 #bluetooth config
 target_name = "Europa"
 #Europa = 'D8:68:C3:94:47:DE' #tablet
-Europa = '34:2D:0D:AD:D4:1D'
+Europa = '94:76:B7:E3:11:CC'
+# Europa = '34:2D:0D:AD:D4:1D'
 # holds the MAC address for the Device
 
 # Raspberry Pi GPIO configuration.
@@ -28,7 +29,7 @@ DC = 24
 RST = 25
 SPI_PORT = 0
 SPI_DEVICE = 0
-backlight =4
+backlight = 3
 UV = 2
 SPEED_HZ = 4000000
 
@@ -62,22 +63,36 @@ disp.begin()
 
 
 def get_image(savePath):
+    global GPIOcontroller
     try:
+        GPIOcontroller.output(backlight, GPIO.HIGH)
         img = camera.GrabOne(4000)
         img = cv2.cvtColor(img.Array, cv2.COLOR_RGB2BGR)
         print("image taken")
         cv2.imwrite(savePath, img)
         print("image saved to " + savePath)
+        GPIOcontroller.output(backlight, GPIO.LOW)
         # return img
     
     except Exception:
         sys.exit("Camera is unconnected or uninitialized. Check connection or implementation")
 
 def initialize_camera():
-    cam= pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+## add a try catch block for camera first line
+    cam = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
     print("Camera Connected : ", cam.GetDeviceInfo().GetModelName())
     cam.Open()
+    cam.ExposureTime.SetValue(400)
+    cam.Width.SetValue(1750)
+    cam.Height.SetValue(1684)
+    cam.OffsetX.SetValue(430)
+    cam.OffsetY.SetValue(260)
+    '''
+    pylon.FeaturePersistence.Save('initial.pfs', cam.GetNodeMap())
+    pylon.FeaturePersistence.Load('NodeMap.pfs', cam.getNodeMap(), False)
+    '''
     cam.PixelFormat.SetValue('RGB8')
+    
     return cam
 
 def check_connection(device_name = "Europa"):
@@ -126,33 +141,36 @@ def calibration_screen():
     disp.display(image)
 
 
-def open_ellipse(x, y, w, h):
+def open_ellipse(x, y, w, h, outline = True):
     # x,y refer to the center point of the ellipse
     # w and h are height and width in mm, limited by min pixel size for ST_7735 (230 microns)
-    
     width = float(w)
     height = float(h)
     a = width / 2.0
     b = height / 2.0
     pix_w =  mm_to_pix(a) 
     pix_h = mm_to_pix(b)
-    print("radius == " + str(pix_h))
-    image = Image.new("P", (WIDTH, HEIGHT), 0)
-    pen =  ImageDraw.Draw(image)
-    pen.rectangle([(x0,y0), (x0 + calibrationWidth, y0 + calibrationHeight)], fill = 0, outline = 255)
+    print("pixel radius == " + str(pix_h))
+    
+    if (outline):
+        image = Image.new("P", (WIDTH, HEIGHT), 255)
+    else:
+        image = Image.new("P", (WIDTH, HEIGHT), 0)
+    pen = ImageDraw.Draw(image)
+    #pen.rectangle([(x0,y0), (x0 + calibrationWidth, y0 + calibrationHeight)], fill = 0, outline = 255)
     if(pix_w <= 1 or pix_h <= 1):
         print("opening point")
         pen.point([x,y] , fill = 255)
     else:
        print("Opening circle")
-       pen.ellipse([(x - pix_w, y - pix_h), (x + pix_w, y + pix_h)], fill = 255, outline = 255)
+       pen.ellipse([(x - pix_w, y - pix_h), (x + pix_w, y + pix_h)], fill = 255, outline = 0)
     image.save('last ROI.bmp')
     disp.display(image)
     
-def open_circle(x, y, diam):
+def open_circle(x, y, diam, outline = True):
     # x,y  refer to the center of the circle
     # diam refers to the diameter in mm, limited by min pixel size for ST_7735 (230 microns)
-    open_ellipse(x,y, diam, diam)
+    open_ellipse(x,y, diam, diam, outline)
 
 def open_rect(x, y, w, h):
     image = Image.new("P", (WIDTH, HEIGHT), 0)
@@ -166,6 +184,9 @@ def open_rect(x, y, w, h):
     else:
         pen.rectangle([(x - w, y - h), (x + w, y + h)], fill = 255)
     disp.display(image)
+    
+def open_square(x,y,w):
+    open_rect(x, y, w, w)
 
 def complex_Mask(points):
     image = Image.new("P", (WIDTH, HEIGHT), 0);
@@ -177,7 +198,7 @@ def start_bluetooth_server(server_sock):
     server_sock.bind(("", port))
     server_sock.listen(1)
     print("listening for connections")
-    uuid = "00001101-0000-1000-8000-00805f9b34fb"
+    uuid = "00000000-0000-1000-8000-00805f9b34fb"
     bluetooth.advertise_service(server_sock, "Europa Interface",service_id = uuid, service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
                    profiles=[bluetooth.SERIAL_PORT_PROFILE])
     client_sock, address = server_sock.accept()
@@ -194,7 +215,6 @@ def start_bluetooth_server(server_sock):
     return client_sock
 
 def start(controller):
-    controller.output(backlight, GPIO.HIGH)
     calibration_screen()
 
 def listen(sock,controller):
@@ -212,6 +232,7 @@ def listen(sock,controller):
     # choose action based on the command
 
 def decision_tree(char, controller, sock):
+    controller.output(backlight, GPIO.LOW)
     print("command == " + char)
     if (char == 'd'):
         openROI(sock, controller)
@@ -227,6 +248,7 @@ def decision_tree(char, controller, sock):
     elif(char == 'q'):
         sock.send("Shutting Down")
         sys.exit()
+    
     listen(sock,controller)
 
 def openROI(sock, controller):
@@ -235,23 +257,28 @@ def openROI(sock, controller):
     x = int.from_bytes(x, byteorder = 'big')
     x = float (x)
     x = x / 100.0
+    x = 1.0 - x
     print ("X: " + str(x))
     y = sock.recv(1)
     y = int.from_bytes(y, byteorder = 'big')
     y = float(y)
     y = y / 100.0
+    y = 1.0 - y
     print("Y: " + str(y))
     X = int(math.floor(x0 + (x * calibrationWidth)))
     Y = int(math.floor(y0 + (y * calibrationHeight)))
-    #print("x: " + X + ", y: " + Y)
+    print("pixel local (x,y) : (" + str(X) + ", " + str(Y) + ")")
     print("Open ROI DIAM: " + str(current_diam));
-    open_circle(X, Y, current_diam)
+    open_circle(X, Y, current_diam, outline = True)
+    controller.output(backlight, GPIO.HIGH)
     # how do I know when to abort during an illumination period
     proceed = sock.recv(1)
     proceed = proceed.decode('ascii')
     print ("Proceed command == " + str (proceed))
+    controller.output(backlight, GPIO.LOW)
     if (proceed == 'l'):
-        illuminate(180, controller, sock)
+        open_circle(X, Y, current_diam, outline = False)
+        illuminate(10, controller, sock)
     elif (proceed == 'm'):
         print("user opted out of illumination, use app to retake picture")
     clear_buffer = sock.recv(1024)
@@ -263,7 +290,7 @@ def settings():
     global current_diam
     print("settings command == " + command)
     if (command == 'a'):
-        current_diam = .2
+        current_diam = .230
     elif (command == 'b'):
         current_diam = default_diam
     elif (command == 'c'):
@@ -299,12 +326,12 @@ def mm_to_pix(mm):
     pix = int(math.floor(mm_float / .230))
     return pix
 
-calibration_screen()
+
+#calibration_screen()
 # Initialize camera and image storage location
 camera = initialize_camera()
 #Europa = check_connection()
 file_location = '/home/pi/Documents/Europa.jpg'
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 europa_sock = start_bluetooth_server(server_sock)
-europa_sock.send("AAAHHHHHHHHHH")
 listen(europa_sock, GPIOcontroller)
